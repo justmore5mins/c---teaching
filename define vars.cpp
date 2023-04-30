4c00896bd518c8399fdd9df80ebee43166c95442
justmore5mins <standby-nexuses0m@icloud.com>
2023-05-01 06:49:09 +0800

    add

---
+++ define vars.cpp
@@ -0,0 +1,22 @@
+//define variables
+int thing = 1; //定義整數
+float thing = 1.0;  //定義多精度浮點數
+bool thing = true; //定義布林職
+
+//define constant
+const int thing = 1; // 定義常數整數
+const float thing = 1.0; //定義常數多精度浮點數
+const char* thing = "j"; //定義常數字符
+const bool thing = true; //定義常數布林職
+
+//Universal
+#define thing 1 //這個可以定義所有變數,但是常數還是要照常用
+
+//--------------------------------------------------------
+
+//註解
+//我是單航註解
+/*
+我是多行註解
+*/
+
